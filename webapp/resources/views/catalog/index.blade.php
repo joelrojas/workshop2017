67f871132d8b98bafaa2bdeaac1e84b6191249d7
ecampohermoso <ecampohermoso@cirrus-it.net>
2017-09-14 09:43:40 -0400

    First version of template

--- webapp/resources/views/catalog/index.blade.php
+++ webapp/resources/views/catalog/index.blade.php
@@ -1,21 +1,21 @@
-<html>
-<head>
-</head>
-<body>
-	<h1>Listado de Catalgos</h1>
-	<table>
-		<th>
-			<td>Acciones</td>
-			<td>Código</td>
-			<td>Descripción</td>
-		</th>
-		@foreach ($catalogList as $catalog)
-		<tr>
-			<td></td>
-			<td>{{ $catalog->name}}</td>
-			<td>{{ $catalog->description}}</td>
-		</tr>
-		@endforeach
-	</table>	
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Listado de Catalogos')
+@section('title-description', 'Administración de los catalogos del sistema')
+
+@section('content')
+<table>
+	<th>
+		<td>Acciones</td>
+		<td>Código</td>
+		<td>Descripción</td>
+	</th>
+	@foreach ($catalogList as $catalog)
+	<tr>
+		<td></td>
+		<td>{{ $catalog->name}}</td>
+		<td>{{ $catalog->description}}</td>
+	</tr>
+	@endforeach
+</table>	
+@endsection
