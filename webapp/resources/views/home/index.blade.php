fb5bbe2b757a5a258c70424f269bead848d8bdef
joelrojas <joel.a.rojas.v@gmail.com>
2017-09-29 01:15:53 -0400

    Creando el controlador (HomeController) y la vista (home/index.blade.php) para la pantalla inicial al loguearse al sistema

---
+++ webapp/resources/views/home/index.blade.php
@@ -0,0 +1,21 @@
+<!-- /**
+ * Created by PhpStorm.
+ * User: joel
+ * Date: 29-09-17
+ * Time: 12:36 AM
+ */-->
+@extends('layouts.app')
+
+@section('menu_dashboard', 'open active')
+@section('title', 'Inicio')
+@section('title-description', 'Página Principal')
+@section('css')
+
+@section('content')
+
+@endsection
+
+@section('js')
+    <script src="js/vendor.js"></script>
+    <script src="js/app-template.js"></script>
+@endsection
