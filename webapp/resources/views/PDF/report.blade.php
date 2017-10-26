7964fb30eb0b51243f4e42a41f3337e35283dc77
saulquiroz <saul85dt@gmail.com>
2017-10-26 00:03:50 -0400

    pdf vista mejorada

--- webapp/resources/views/PDF/report.blade.php
+++ webapp/resources/views/PDF/report.blade.php
@@ -10,25 +10,27 @@
 </head>
 <body topmargin="0" leftmargin="0" marginwidth="0" marginheight="0" scroll="no" style="overflow:hidden">
 <h1>Lista de tareas</h1>
+Tareas de
         <div >
             <div class="content">
                 <table border="2" style="table-layout: fixed; width: 350px;">
                     <thead>
                     <tr>
-                        <th>Estado</th>
-                        <th>Fecha de Salida</th>
-                        <th>Fecha de inicio</th>
+                        <th>Apellido</th>
                         <th>Nombre</th>
+                        <th>Estado</th>
+                        <th>Fecha de inicio</th>
+                        <th>Fecha de Salida</th>
                     </tr>
                     </thead>
                     <tbody>
                     @foreach ($user as $key)
                         <tr>
-
-                            <td>{{ $key->state }}</td>
-                            <td>{{ $key->dateEnd }}</td>
-                            <td>{{ $key->dateBegin }}</td>
+                            <td>{{ $key->lastName }}</td>
                             <td>{{ $key->name }}</td>
+                            <td>{{ $key->state }}</td>
+                            <td>{{ $key->dateBegin }}</td>
+                            <td>{{ $key->dateEnd }}</td>
                         </tr>
                     @endforeach
                     </tbody>
