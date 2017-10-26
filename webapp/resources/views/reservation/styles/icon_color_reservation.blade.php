3b96e22a8c0e5d0b151d2c8d76fc28e69004eb87
Joel Andy Rojas Valencia <joel.a.rojas.v@gmail.com>
2017-10-26 01:10:22 -0400

    FrontEnd: reservation/styles
    se modifico las condicionales para mostrar el respectivo estado de la reserva en la vista de la informacion de la reserva.

--- webapp/resources/views/reservation/styles/icon_color_reservation.blade.php
+++ webapp/resources/views/reservation/styles/icon_color_reservation.blade.php
@@ -1,2 +1,9 @@
-<?php
-
+@if($reservation->state_reservation == 'cancelado')
+    <div class="icon-big icon-danger text-center">
+        @elseif($reservation->state_reservation == 'en espera')
+            <div class="icon-big icon-warning text-center">
+                @elseif($reservation->state_reservation == 'completado')
+                    <div class="icon-big icon-success text-center">
+                        @else
+                            <div class="icon-big icon-info text-center">
+@endif
--- webapp/resources/views/reservation/styles/information_reservation.blade.php
+++ webapp/resources/views/reservation/styles/information_reservation.blade.php
@@ -1,4 +1,76 @@
-@if($reservation->state_reservation == 'completado')
+@if($reservation->state_reservation == 'cancelado')
+    <div class="row" align="center">
+        <div class="col-lg-7 col-sm-6 col-lg-offset-3">
+            <div class="card" align="center">
+                <div class="card-content">
+                    <div class="alert alert-danger">
+                        <br><h4><b> RESERVA CANCELADA O ANULADA</b></h4>
+                    </div>
+                    <h5 class="big-title"><b>Descripción :</b> no se presento a la reserva, <br>se llamo al cliente y no contesto el celular.</h5>
+                    <div id="chartTotalSubscriptions"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+@elseif($reservation->state_reservation == 'en espera')
+        <div class="row" align="center">
+
+            <div class="col-lg-7 col-sm-6 col-lg-offset-3">
+                <div class="card" align="center">
+                    <div class="card-content">
+                        <div class="alert alert-warning">
+                            <br><h4><b> ESPERANDO AL CLIENTE</b></h4>
+                        </div>
+                        <h5 class="big-title"><b>NOMBRE DE LA MESA RESERVADA :</b> {{$reservation->nameTable}}</h5>
+                        <div id="chartTotalSubscriptions"></div>
+                    </div>
+                    <div class="card-footer">
+                        <hr>
+                        <div class="footer-title"><b>TIPO DE MESA RESERVADA :</b> {{ $reservation->typeTable }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @elseif($reservation->state_reservation == 'completado')
+                <div class="row">
+                    <div class="card-header">
+                        <h3 class="card-title" ><b>DATOS DE LA FACTURA</b>
+                            <a onclick="addCatalog()" class="btn btn-group btn-default btn-wd btn-fill pull-right" style="margin-top: -8px"><i class="ti-settings"></i> VER FACTURA</a>
+                        </h3>
+                        <hr>
+                    </div>
+                    <div class="col-md-12">
+                        <div align="center">
+                            <table id="catalog-table" class="table table-striped">
+                                <thead class="thead-inverse">
+                                <tr>
+                                    <th><b>Codigo</b></th>
+                                    <th><b>Cantidad</b></th>
+                                    <th><b>Producto</b></th>
+                                    <th><b>Precio Uni.</b></th>
+                                    <th><b>Precio Total</b></th>
+                                    <!-- <th><b>Acciones</b></th> -->
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr role="row" class="even">
+                                    <td class="sorting_1">6</td>
+                                    <td>3</td>
+                                    <td>Jack daniels</td>
+                                    <td>300</td>
+                                    <td>900</td>
+                                    <!-- <td><div class="table-icons"><a onclick="editCatalog(6)" class="btn btn-primary btn-group-xs btn-fill "><i class="ti ti-marker"></i> Editar</a><a onclick="deleteCatalog(6)" class="btn btn-danger btn-group-xs btn-fill "><i class="ti ti-trash"></i> Eliminar</a></div></td></tr> -->
+                                </tr>
+                                <tr>
+                                    <td colspan="4" style="text-align: right"><b>TOTAL: </b></td>
+                                    <td><b>900 Bs.</b></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                @else
     <div class="row">
         <div class="card-header">
             <h3 class="card-title" ><b>DATOS DE LA FACTURA</b>
@@ -16,7 +88,7 @@
                         <th><b>Producto</b></th>
                         <th><b>Precio Uni.</b></th>
                         <th><b>Precio Total</b></th>
-                        <!-- <th><b>Acciones</b></th> -->
+                        <th><b>Acciones</b></th>
                     </tr>
                     </thead>
                     <tbody>
@@ -26,7 +98,7 @@
                         <td>Jack daniels</td>
                         <td>300</td>
                         <td>900</td>
-                        <!-- <td><div class="table-icons"><a onclick="editCatalog(6)" class="btn btn-primary btn-group-xs btn-fill "><i class="ti ti-marker"></i> Editar</a><a onclick="deleteCatalog(6)" class="btn btn-danger btn-group-xs btn-fill "><i class="ti ti-trash"></i> Eliminar</a></div></td></tr> -->
+                        <td><div class="table-icons"><a onclick="editCatalog(6)" class="btn btn-primary btn-group-xs btn-fill "><i class="ti ti-marker"></i> Editar</a><a onclick="deleteCatalog(6)" class="btn btn-danger btn-group-xs btn-fill "><i class="ti ti-trash"></i> Eliminar</a></div></td></tr>
                     </tr>
                     <tr>
                         <td colspan="4" style="text-align: right"><b>TOTAL: </b></td>
@@ -37,4 +109,4 @@
             </div>
         </div>
     </div>
-@elseif
+@endif
