e8c7b673170ca186732b33ec07583c689cacaea8
Joel Andy Rojas Valencia <joel.a.rojas.v@gmail.com>
2017-11-09 00:58:55 -0400

    FrontEnd: auth/login
    Modificando la vista de login.

--- webapp/resources/views/auth/login.blade.php
+++ webapp/resources/views/auth/login.blade.php
@@ -1,104 +1,148 @@
+<!--  -->
+
+
 
 <!doctype html>
-<html class="no-js" lang="en">
+<html lang="en">
+
+<!-- Mirrored from demos.creative-tim.com/paper-dashboard-pro/examples/pages/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 17 Oct 2017 00:18:10 GMT -->
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title> ModularAdmin - Free Dashboard Theme | HTML Version </title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <!-- Place favicon.ico in the root directory -->
-    <link rel="stylesheet" href="css/vendor.css">
-    <link rel="stylesheet" id="theme-style" href="css/app.css">
-    <!-- Theme initialization -->
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/img/favicon.png') }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Club Gitana</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Bootstrap core CSS     -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="../assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Paper Dashboard core CSS    -->
+    <link href="../assets/css/paper-dashboard.css" rel="stylesheet"/>
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../assets/css/demo.css" rel="stylesheet" />
+
+    <!--  Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
+    <link href="../assets/css/themify-icons.css" rel="stylesheet">
 </head>
+
 <body>
-<div class="auth">
-    <div class="auth-container">
-        <div class="card">
-            <header class="auth-header">
-                <h1 class="auth-title">
-                    <div class="logo">
-                        <span class="l l1"></span>
-                        <span class="l l2"></span>
-                        <span class="l l3"></span>
-                        <span class="l l4"></span>
-                        <span class="l l5"></span>
-                    </div> Club Gitanas </h1>
-            </header>
-            <div class="auth-content">
-                <p class="text-center">INICIAR SESIÓN</p>
-                <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                    {{ csrf_field() }}
+<nav class="navbar navbar-transparent navbar-absolute">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="../dashboard/overview.html">CLUB GITANAS</a>
+        </div>
+        <!-- <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="register.html">
+                        Register
+                    </a>
+                </li>
+                <li>
+                    <a href="../dashboard/overview.html">
+                        Dashboard
+                    </a>
+                </li>
+            </ul>
+        </div> -->
+    </div>
+</nav>
 
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class="col-md-6 control-label">E-Mail Address</label>
-
-                        <div class="col-md-12">
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                            @if ($errors->has('email'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password" class="col-md-4 control-label">Password</label>
-
-                        <div class="col-md-12">
-                            <input id="password" type="password" class="form-control" name="password" required>
-
-                            @if ($errors->has('password'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                </label>
+<div class="wrapper wrapper-full-page">
+    <div class="full-page login-page" data-color="" data-image="http://demos.creative-tim.com/paper-dashboard-pro/assets/img/background/background-2.jpg">
+        <!--   you can change the color of the filter page using: data-color="blue | azure | green | orange | red | purple" -->
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
+                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+                            <div class="card" data-background="color" data-color="blue">
+                                <div class="card-header">
+                                    <h3 class="card-title"><b>INICIAR SESIÓN</b></h3>
+                                </div>
+                                <div class="card-content">
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <label for="email" class="col-md-5 control-label">Correo Electronico</label>
+                                        <div class="col-md-12">
+                                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                            @if ($errors->has('email'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                 </span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <label for="password" class="col-md-3 control-label">Contraseña</label>
+                                        <div class="col-md-12">
+                                            <input id="password" type="password" class="form-control" name="password" required>
+                                            @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer text-center">
+                                    <button type="submit" class="btn btn-fill btn-wd ">Entrar</button>
+                                    <!-- <div class="forgot">
+                                        <a href="#pablo">Forgot your password?</a>
+                                    </div> -->
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
-
-                    <div class="form-group">
-                        <div class="col-md-8 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
-                                Login
-                            </button>
-
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                Forgot Your Password?
-                            </a>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
-        <div class="text-center">
-            <a href="index.html" class="btn btn-secondary btn-sm">
-                <i class="fa fa-arrow-left"></i> Back to dashboard </a>
-        </div>
+
+        <footer class="footer footer-transparent">
+            <div class="container">
+                <div class="copyright">
+                    PROYECTO DE TALLER DE SISTEMAS
+                </div>
+            </div>
+        </footer>
     </div>
 </div>
-<!-- Reference block for JS -->
-<div class="ref" id="ref">
-    <div class="color-primary"></div>
-    <div class="chart">
-        <div class="color-primary"></div>
-        <div class="color-secondary"></div>
-    </div>
-</div>
-<script src="js/vendor.js"></script>
-<script src="js/app.js"></script>
 </body>
+
+<script src="{{ asset('assets/js/jquery-1.10.2.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/jquery-ui.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/perfect-scrollbar.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/demo.js') }}" type="text/javascript"></script>
+
+
+<script type="text/javascript">
+    $().ready(function(){
+        demo.checkFullPageBackgroundImage();
+
+        setTimeout(function(){
+            // after 1000 ms we add the class animated to the login/register card
+            $('.card').removeClass('card-hidden');
+        }, 700)
+    });
+</script>
+
+
+<!-- Mirrored from demos.creative-tim.com/paper-dashboard-pro/examples/pages/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 17 Oct 2017 00:18:10 GMT -->
 </html>
